f35deedc74b31f3c161e6ba53e945485244223c8
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-03 21:15:50 +0900

    update: user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'DJ HIRROT', email: 'dj_hirrot@auth.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
 end
