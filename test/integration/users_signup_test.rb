f93249295181a5e5eda7842e5c38fd8a8df4c88b
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-12 00:27:36 +0900

    update: added test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get join_path
 
     assert_no_difference 'User.count' do
-      post users_path, params: { user: {
+      post users_path, params: {
+        user: {
           name: '',
           email: 'user@invalid',
           password: 'foo',
@@ -17,5 +18,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert.alert-danger'
+  end
+
+  test "valid signup information" do
+    get join_path
+
+    assert_difference 'User.count', 1 do
+      post join_path, params: {
+        user: {
+          name: "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+          }
+        }
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
