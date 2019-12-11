cf27d74e79bf3c9ad4e0616bb5fbce2fc71c24c5
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-12 00:08:28 +0900

    update: modified path

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
-    get new_user_path
+    get join_path
 
     assert_no_difference 'User.count' do
       post users_path, params: { user: {
