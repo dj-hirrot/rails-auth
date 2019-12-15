e96bed6863b974278715cee621bd5f01e0a87426
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-15 17:01:40 +0900

    add: created sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/join', to: 'users#new'
   post '/join', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources 'users'
 end
