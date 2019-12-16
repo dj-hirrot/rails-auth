78eaa9c9c1aad5a88f101ece652de82bb0adf560
dj-hirrot <hirotoshibutani@gmail.com>
2019-12-16 22:47:27 +0900

    fix: type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      log_in @user
+      login @user
       flash[:success] = 'Join success!!'
       redirect_to @user
     else
